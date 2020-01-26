11e4853d752afdbc3fccf50c66c6811cd62d2d9f
kpsurmay <60282636+kpsurmay@users.noreply.github.com>
2020-01-26 13:20:01 -0500

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -3,21 +3,21 @@ require('function.php');
 if(isset($_POST['aceptar']))
 {
 	$cedula=$_POST['id_date_person'];
-  $tipo_doc=$POST['type_doc'];
+  $tipo_doc=$_POST['id_type_doc'];
 	$nombre=$_POST['f_name'];
 	$nombre2=$_POST['s_name'];
 	$apellido=$_POST['I_name'];
 	$apellido2=$_POST['I_name_s'];
   $foto=$_POST['photo'];
 	$fecha_nac=$_POST['date'];
-  $sangre=$_POST['blood_type'];
-  $genero=$_POST[gender];
-	$rh=$_POST['rh'];
-	$estado_c=$_POST['matrial_status'];
-	$raza=$_POST['race'];
-  $ciudad_nac=$_POST['city_born'];
-  $eps=$_POST['eps'];
-	$estrato=$_POST['score'];
+  $sangre=$_POST['id_blood_type'];
+  $genero=$_POST['id_gender'];
+	$rh=$_POST['id_rh'];
+	$estado_c=$_POST['id_matrial_status'];
+	$raza=$_POST['id_race'];
+  $ciudad_nac=$_POST['id_city_born'];
+  $eps=$_POST['id_eps'];
+	$estrato=$_POST['id_score'];
 	
 	$sql="INSERT INTO date_person VALUES ($cedula,'$tipo_doc','$nombre','$nombre2','$apellido','$apellido2','$foto','$fecha_nac',$sangre,$genero,$rh,$estado_c,$ciudad_nac,$raza,$eps,$estrato)";
 
@@ -31,7 +31,7 @@ if(isset($_POST['aceptar']))
 <div class="form-group">
   <label class="col-md-4 control-label" for="city">Cedula</label>  
   <div class="col-md-4">
-  <input id="cedula" name="id_date_person" placeholder="123456" class="form-control input-md" required="" type="number">
+  <input id="id_date_person" name="id_date_person" placeholder="123456" class="form-control input-md" required="" type="number">
     
   </div>
 </div>
@@ -47,7 +47,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="rh" class="form-control" >
+        <select style="height: 35px;" name="id_type_doc" class="form-control" >
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_type_doc'].'">'.$per['type_doc']. '</option>';
         echo "<br>";
@@ -77,7 +77,7 @@ if(isset($_POST['aceptar']))
 <div class="form-group">
   <label class="col-md-4 control-label" for="city">Primer Apellido</label>  
   <div class="col-md-4">
-  <input id="city" name="I_namee" placeholder="Pallares" class="form-control input-md" required="" type="text">
+  <input id="city" name="I_name" placeholder="Pallares" class="form-control input-md" required="" type="text">
     
   </div>
 </div>
@@ -118,7 +118,7 @@ if(isset($_POST['aceptar']))
 	   ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="tipo_sa" class="form-control" >
+        <select style="height: 35px;" name="id_blood_type" class="form-control" >
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_blood_type'].'">'.$per['blood_type']. '</option>';
         echo "<br>";
@@ -140,7 +140,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_gender" class="form-control" >
     <option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_gender'].'">'.$per['gender'].'</option>';
@@ -163,7 +163,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_rh" class="form-control" >
     <option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_rh'].'">'.$per['rh'].'</option>';
@@ -186,7 +186,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_matrial_status" class="form-control" >
     <option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_matrial_status'].'">'.$per['matrial_status'].'</option>';
@@ -209,7 +209,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_race" class="form-control" >
     <option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_race'].'">'.$per['race'].'</option>';
@@ -232,7 +232,7 @@ if(isset($_POST['aceptar']))
      ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_city_born" class="form-control" >
     <option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_city_born'].'">'.$per['city_born'].'</option>';
@@ -255,7 +255,7 @@ if(isset($_POST['aceptar']))
 	   ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="eps2" class="form-control" >
+        <select style="height: 35px;" name="id_eps" class="form-control" >
 		<option value="'0">Seleccione</option>
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_eps'].'">'.$per['eps'].'</option>';
@@ -277,7 +277,7 @@ if(isset($_POST['aceptar']))
 	   ?>
          
         <div class="col-xs-12 col-sm-6 col-md-6">
-        <select style="height: 35px;" name="institucion" class="form-control" required="" >
+        <select style="height: 35px;" name="id_score" class="form-control" required="" >
         <?php foreach ($pers as $per) {
         echo '<option value="'.$per['id_score'].'">'.$per['score'].'</option>';
         echo "<br>";
